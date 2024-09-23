7edbc7325344376b62e1a28664dcce0e2b5428a3
Kazuma Ito <zarigani2x@gmail.com>
2024-09-24 01:44:37 +0900

    Refactor .env file ignore patterns

--- src/app/(app)/workouts/Workout.tsx
+++ src/app/(app)/workouts/Workout.tsx
@@ -1,14 +1,23 @@
 import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import client from "@/lib/hono";
+import { cn } from "@/lib/utils/utils";
 import dayjs from "dayjs";
 import { InferResponseType } from "hono";
+import {
+  ActivityIcon,
+  CalendarIcon,
+  ClockIcon,
+  MapPinIcon,
+} from "lucide-react";
 import Link from "next/link";
 
 type Props = {
   workout: InferResponseType<typeof client.api.workout.$get, 200>[0];
+  isRecent?: boolean;
 };
 
-const Workout = ({ workout }: Props) => {
+const Workout = ({ workout, isRecent }: Props) => {
   const exerciseNames = Array.from(
     new Set(
       workout.sets.map((set) => {
@@ -18,22 +27,49 @@ const Workout = ({ workout }: Props) => {
   );
 
   return (
-    <Link
-      href={`/workouts/${workout.id}`}
-      className="block w-full rounded-lg border p-4"
-    >
-      <div className="flex items-center gap-1">
-        <div className="">{dayjs(workout.date).format("MMM DD")}</div>・
-        <div>{workout.location?.name}</div>
-      </div>
-      <div className="mt-2 flex flex-wrap gap-1">
-        {exerciseNames.length === 0 && (
-          <div className="text-neutral-400">No exercises</div>
-        )}
-        {exerciseNames.map((exerciseName, i) => {
-          return <Badge key={i}>{exerciseName}</Badge>;
-        })}
-      </div>
+    <Link href={`/workouts/${workout.id}`}>
+      <Card className="rounded-none shadow-none md:rounded-lg">
+        <CardHeader>
+          <CardTitle className={cn("flex items-center gap-4 font-bold")}>
+            <div
+              className={cn(
+                isRecent &&
+                  "bg-gradient-to-r from-blue-600 to-cyan-500 bg-clip-text text-transparent",
+              )}
+            >
+              {dayjs(workout.date).format("MMM D")}
+            </div>
+            {isRecent && <Badge className="ml-auto">Recent</Badge>}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-2 md:flex-row md:items-center md:gap-4">
+          <div className="flex items-center space-x-2 text-sm text-neutral-500">
+            <ClockIcon className="size-4" />
+            <div className="font-semibold">
+              {dayjs(workout.date).format("h:mm A")}
+            </div>
+          </div>
+          <div className="flex items-center space-x-2 text-sm text-neutral-500">
+            <MapPinIcon className="size-4" />
+            <div className="font-semibold">{workout.location?.name}</div>
+          </div>
+          <div className="flex items-center space-x-2 text-sm text-neutral-500">
+            <ActivityIcon className="size-4" />
+            <div className="flex w-full items-center gap-1 overflow-auto">
+              {exerciseNames.length === 0 && (
+                <div className="text-neutral-400">No exercises</div>
+              )}
+              {exerciseNames.map((exerciseName, i) => {
+                return (
+                  <Badge variant="outline" key={i} className="shrink-0">
+                    {exerciseName}
+                  </Badge>
+                );
+              })}
+            </div>
+          </div>
+        </CardContent>
+      </Card>
     </Link>
   );
 };
--- src/app/(app)/workouts/WorkoutList.tsx
+++ src/app/(app)/workouts/WorkoutList.tsx
@@ -12,80 +12,70 @@ import { Skeleton } from "@/components/ui/skeleton";
 import client from "@/lib/hono";
 import { InferResponseType } from "hono";
 import { Plus } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Workout from "./Workout";
 import WorkoutForm from "./WorkoutForm";
 import { cn } from "@/lib/utils/utils";
-import InfiniteScroll from "@/components/InfiniteScroll";
-import { Spinner } from "@/components/myui/spinner";
 
 const WorkoutList = () => {
   const [workouts, setWorkouts] = useState<
     InferResponseType<typeof client.api.workout.$get, 200>
   >([]);
-  const [isInitLoading, setIsInitLoading] = useState(true);
-  const [noMore, setNoMore] = useState(false);
-  const [offset, setOffset] = useState(0);
-  const fetched = useRef(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const fetchWorkouts = async () => {
     const res = await client.api.workout.$get({
       query: {
         limit: "15",
-        offset: offset.toString(),
+        offset: "0",
       },
     });
     if (!res.ok) {
       return;
     }
     const data = await res.json();
+
+    setIsLoading(false);
+
     if (data.length === 0) {
-      setNoMore(true);
       return;
     }
-    setOffset((prev) => prev + data.length);
-    setWorkouts((prev) => [...prev, ...data]);
-    setIsInitLoading(false);
+    setWorkouts(data);
   };
 
   useEffect(() => {
-    if (!fetched.current) {
-      fetched.current = true;
-      fetchWorkouts();
-    }
+    fetchWorkouts();
   }, []);
 
   return (
-    <div className="flex flex-col items-center px-4">
-      {isInitLoading && (
-        <div className="flex w-full flex-col gap-2">
-          {Array.from({ length: 6 }).map((_, i) => (
-            <Skeleton className="h-24 w-full shrink-0 rounded-xl" key={i} />
+    <div className="flex flex-col items-center md:px-4">
+      {isLoading && (
+        <div className="flex w-full flex-col gap-1 md:gap-2">
+          {Array.from({ length: 5 }).map((_, i) => (
+            <Skeleton
+              className="h-[170px] w-full shrink-0 md:h-[120px] md:rounded-xl"
+              key={i}
+            />
           ))}
         </div>
       )}
-      {!isInitLoading && workouts.length === 0 ? (
+      {!isLoading && workouts.length === 0 ? (
         <div className="py-20 text-neutral-400">
           Let's add your first workout!
         </div>
       ) : null}
-      <InfiniteScroll
-        onMore={fetchWorkouts}
-        noMore={noMore}
-        className="flex w-full flex-col gap-2 overflow-auto"
-        loadingContent={<Spinner className="mx-auto my-4" />}
-      >
+      <div className="flex w-full flex-col -space-y-1 overflow-auto md:space-y-2">
         {workouts.map((workout, i) => {
-          return <Workout workout={workout} key={i} />;
+          return <Workout workout={workout} isRecent={i === 0} key={i} />;
         })}
-      </InfiniteScroll>
-      <div className="sticky bottom-16 mt-4 w-full md:bottom-4">
+      </div>
+      <div className="sticky bottom-16 mt-4 w-full px-4 md:bottom-4">
         <Dialog>
           <DialogTrigger asChild>
             <Button
               className={cn(
                 "w-full rounded-full opacity-0 shadow-lg",
-                !isInitLoading && "animate-fade-in-up",
+                !isLoading && "animate-fade-in-up",
               )}
               size="lg"
             >
